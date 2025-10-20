4050b885035d8a0735e3b7a1759e78dd27c798fc
Herrera2323 <152764148+Herrera2323@users.noreply.github.com>
2025-10-19 18:47:26 -0600

    Add files via upload

---
+++ DISEÑOINTERFASES/Conexionbd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace AutosNARLA
+{
+    public class Conexionbd
+    {
+        private static string connectionString = ConfigurationManager
+           .ConnectionStrings["AutosNARLA"]
+           .ConnectionString;
+
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(connectionString);
+            conexion.Open();
+            return conexion;
+        }   
+    }
+}
